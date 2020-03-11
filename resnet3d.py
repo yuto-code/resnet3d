6285ab0a93e9a03a8d01988e6a79aa5007554d65
yuto-code <61311871+yuto-code@users.noreply.github.com>
2020-03-11 13:58:19 +0900

    Update resnet3d.py

--- resnet3d.py
+++ resnet3d.py
@@ -1,6 +1,3 @@
-"""A vanilla 3D resnet based on Raghavendra Kotikalapudi's 2D implementation
-keras-resnet (See https://github.com/raghakot/keras-resnet.)
-"""
 import six
 from keras.models import Model
 from keras.layers import (
@@ -21,9 +18,6 @@ from keras import backend as K
 
 
 def _bn_relu(input):
-    """Helper to build a BN -> relu block (copy-paster from
-    raghakot/keras-resnet)
-    """
     norm = BatchNormalization(axis=CHANNEL_AXIS)(input)
     return Activation("relu")(norm)
 
@@ -49,8 +43,6 @@ def _conv_bn_relu3D(**conv_params):
 
 
 def _bn_relu_conv3d(**conv_params):
-    """Helper to build a  BN -> relu -> conv3d block.
-    """
     filters = conv_params["filters"]
     kernel_size = conv_params["kernel_size"]
     strides = conv_params.setdefault("strides", (1, 1, 1))
@@ -70,9 +62,6 @@ def _bn_relu_conv3d(**conv_params):
 
 
 def _shortcut3d(input, residual):
-    """3D shortcut to match input and residual and merges them with "sum" at
-    the same time
-    """
     stride_dim1 = input._keras_shape[DIM1_AXIS] \
         // residual._keras_shape[DIM1_AXIS]
     stride_dim2 = input._keras_shape[DIM2_AXIS] \
@@ -114,11 +103,8 @@ def _residual_block3d(block_function, filters, repetitions,
 
 def basic_block(filters, strides=(1, 1, 1),
                 is_first_block_of_first_layer=False):
-    """Basic 3 X 3 X 3 convolution blocks. Extended from raghakot's 2D impl.
-    """
     def f(input):
         if is_first_block_of_first_layer:
-            # don't repeat bn->relu since we just did bn->relu->maxpool
             conv1 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
                            strides=strides, kernel_initializer="he_normal",
                            padding="same", kernel_regularizer=l2(5e-3)
@@ -138,11 +124,8 @@ def basic_block(filters, strides=(1, 1, 1),
 
 def bottleneck(filters, strides=(1, 1, 1),
                is_first_block_of_first_layer=False):
-    """Basic 3 X 3 X 3 convolution blocks. Extended from raghakot's 2D impl.
-    """
     def f(input):
         if is_first_block_of_first_layer:
-            # don't repeat bn->relu since we just did bn->relu->maxpool
             conv_1_1 = Conv3D(filters=filters, kernel_size=(1, 1, 1),
                               strides=strides, padding="same",
                               kernel_initializer="he_normal",
@@ -189,18 +172,6 @@ def _get_block(identifier):
 class Resnet3DBuilder(object):
     @staticmethod
     def build(input_shape, num_outputs,  block_fn, repetitions):
-        """Instantiate a vanilla ResNet3D keras model
-        # Arguments
-            input_shape: Tuple of input shape in the format
-            (conv_dim1, conv_dim2, conv_dim3, channels) if dim_ordering='tf'
-            (filter, conv_dim1, conv_dim2, conv_dim3) if dim_ordering='th'
-            num_outputs: The number of outputs at the final softmax layer
-            block_fn: Unit block to use {'basic_block', 'bottlenack_block'}
-            repetitions: Repetitions of unit blocks
-        # Returns
-            model: a 3D ResNet model that takes a 5D tensor (volumetric images
-            in batch) as input and returns a 1D vector (prediction) as output.
-        """
 
         _handle_data_format()
         if len(input_shape) != 4:
@@ -212,13 +183,11 @@ class Resnet3DBuilder(object):
 
         block_fn = _get_block(block_fn)
         input = Input(shape=input_shape)
-        # first conv
         conv1 = _conv_bn_relu3D(filters=64, kernel_size=(7, 7, 7),
                                 strides=(2, 2, 2))(input)
         pool1 = MaxPooling3D(pool_size=(3, 3, 3), strides=(2, 2, 2),
                              padding="same")(conv1)
 
-        # repeat blocks
         block = pool1
         filters = 64
         for i, r in enumerate(repetitions):
@@ -227,10 +196,8 @@ class Resnet3DBuilder(object):
                                       )(block)
             filters *= 2
 
-        # last activation
         block_output = _bn_relu(block)
 
-        # average poll and classification
         pool2 = AveragePooling3D(pool_size=(block._keras_shape[DIM1_AXIS],
                                             block._keras_shape[DIM2_AXIS],
                                             block._keras_shape[DIM3_AXIS]),
